84bd727a08320caa35a35a27e451f66fb4ad84d9
Daniel <dgaiteiro@gmail.com>
2023-04-03 17:29:43 +0200

    sobrecarga de operadores

--- 04-SobrecargaDeOperadores/main.cpp
+++ 04-SobrecargaDeOperadores/main.cpp
@@ -1,8 +1,104 @@
 #include <QCoreApplication>
+#include <QDebug>
+
+class Punto2D{
+public:
+    Punto2D(); //constructor sin parámetros
+    Punto2D(double x, double y); //constructor con parámetros
+    Punto2D(const Punto2D &otro); //constructor que recibe otro punto, el constructor de copia
+    ~Punto2D();//destructor
+    void setX(double x);//método set que recibe la x y la modifica
+    void setY(double y);//método set que recibe la y y la modifica
+    double x() const;//método x que devuelve valor de la x
+    double y() const;//método y que devuelve valor de la y
+    void imprime() const;
+private:
+    double m_x; //uso la "m" primero para indicar que es una variable miembro = variable privada
+    double m_y;
+};
+
+//aca creamos el contenido de los métodos
+Punto2D::Punto2D(){ //tenemos que poner el nombre de la clase a cada método para poder hacer la resolución de ámbito
+    m_x = 0.0;
+    m_y = 0.0;
+};
+Punto2D::Punto2D(double x, double y){
+    this->m_x = x;
+    this->m_y = y;
+    // m_x = x;
+    // m_y = y;
+};
+Punto2D::Punto2D(const Punto2D &otro){
+    m_x = otro.m_x;
+    m_y = otro.m_y;
+};
+Punto2D::~Punto2D(){
+    qDebug() << "memoria liberada"; //aca no es necesario poner algo. Suele ir vacio
+};
+void Punto2D::setX(double x){
+    this->m_x = x;
+    //m_x = x;
+};
+void Punto2D::setY(double y){
+    this->m_y = y;
+    m_y = y;
+};
+double Punto2D::x() const{
+    return m_x;
+};
+double Punto2D::y() const{
+    return m_y;
+};
+void Punto2D::imprime() const{
+    qDebug() << m_x << "," << m_y;
+};
+//cuando no existen operadores aritméticos para la clase que trabajamos, tenemos que definir eso nosotros
+//creo un método
+Punto2D operator+(const Punto2D &p, Punto2D &q){
+    Punto2D resultadoDeLaSuma;
+    resultadoDeLaSuma.setX(p.x() + q.x());
+    resultadoDeLaSuma.setY(p.y() + q.y());
+    return resultadoDeLaSuma;
+};
+
+Punto2D operator-(const Punto2D &p, Punto2D &q){ //devuelvo un punto como resultado, el operador es el de resta y recibo por parámetros 2 puntos (p y q)
+    Punto2D resultadoDeLaResta;//declaramos un punto de resultado
+    resultadoDeLaResta.setX(p.x() - q.x());//le ponemos de x, la x de p menos la x de q
+    resultadoDeLaResta.setY(p.y() - q.y());//le ponemos de y, la y de p menos la y de q
+    return resultadoDeLaResta; //devolvemos el resultado
+};
+
+Punto2D operator*(const Punto2D &p, Punto2D &q){
+    Punto2D resultadoDeLaMultiplicacion;
+    resultadoDeLaMultiplicacion.setX(p.x() * q.x());
+    resultadoDeLaMultiplicacion.setY(p.y() * q.y());
+    return resultadoDeLaMultiplicacion;
+};
+
+Punto2D operator/(const Punto2D &p, Punto2D &q){
+    Punto2D resultadoDeLaDivision;
+    resultadoDeLaDivision.setX(p.x() / q.x());
+    resultadoDeLaDivision.setY(p.y() / q.y());
+    return resultadoDeLaDivision;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Punto2D p(1.0, 3.0);
+    Punto2D q(2.0, 5.0);
+
+    Punto2D sumapq = p + q;
+    Punto2D restapq = p - q;
+    Punto2D multiplicacionpq = p * q;
+    Punto2D divisionpq = p / q;
+
+    sumapq.imprime();
+    restapq.imprime();
+    multiplicacionpq.imprime();
+    divisionpq.imprime();
+
 
     return a.exec();
 }
