d3ace67ca5a1130e648d2967027a4277c83f6fdb
Daniel <dgaiteiro@gmail.com>
2023-04-03 15:49:00 +0200

    02-constructores y destructores done

--- 02-ConstructoresyDestructores/main.cpp
+++ 02-ConstructoresyDestructores/main.cpp
@@ -1,7 +1,6 @@
 #include <QCoreApplication>
 #include <QDebug>
 
-
 struct Punto2D{
     double x;
     double y;
@@ -16,24 +15,29 @@ struct Punto2D{
     ~Punto2D();//destructor - solo puede haber un destructor.
     void imprime() const; // método imprime para mostrar el contenido del struct Punto2D
 };
+
 //ahora defino/instancio los médotos/funciones. Declarados arriba.
 Punto2D::Punto2D(){
     x = 0.0; //defino el valor inicial de las variables, antes declaradas.
-    y = 0.0;
+    y = 0.0; //valor inicial de x y y, de la estructura
 };
+
 Punto2D::Punto2D(double x, double y){
     //aca como recibimos parámetros, tenemos que usar el puntero this
     this->x = x; //el "x" de esta estructura" será igual a la "x" del argumento
     this->y = y;
 };
+
 Punto2D::Punto2D(const Punto2D &otro){
     //acá tenemos un constructor que recibe otro punto
-    x = otro.x;
+    x = otro.x; //la x del punto va a ser igual a la x del otro punto
     y = otro.y;
 };
+
 Punto2D::~Punto2D(){
     qDebug() << "punto destruido"; // memoria liberada
 };
+
 void Punto2D::imprime() const{
     qDebug() << x << ", " << y;
 }
@@ -43,9 +47,18 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Punto2D *punto = new Punto2D;
+    Punto2D *punto = new Punto2D;//Esto hace que se ejecute el primer constructor (el sin parámetros)
+    punto->imprime();
+    //libera memoria
+    delete punto; //el destructor me sirve para liberar la memoria con el operador delete. Porque al hacer uso de new, hay uso de memoria dinámica.
 
-    delete punto;
+    Punto2D *punto2 = new Punto2D(2.3, 3.6);//Esto hace que se ejecute el segundo constructor
+    punto2->imprime();
+    delete punto2;
+
+    Punto2D *punto3 = new Punto2D(Punto2D(1.1, 1.5));//constructor que recibe otro punto como parámetro con las siguientes parámetros. Acá es una variable anónima, es un constructor sin nombre.
+    punto3->imprime();
+    delete punto3;
 
     return a.exec();
 }
