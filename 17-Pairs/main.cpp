9cf749f8e88336cfad9517d7a804abca6bf85d55
Daniel <dgaiteiro@gmail.com>
2023-04-11 09:04:44 +0200

    carpetas build generadas automaticamente borradas

---
+++ 17-Pairs/main.cpp
@@ -0,0 +1,56 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <algorithm>
+#include <vector>
+
+//Pairs son plantillas de dos tipos de datos
+
+void imprime(const std::pair<int, QString> &parameter){
+    qDebug() << parameter.first << parameter.second;
+}
+
+void imprime2(const std::pair<QString, QString> &parameter){
+    qDebug() << parameter.first << parameter.second;
+}
+
+//creo la funcion que recibe un vector, de pares, de dos valores QString, llamado english Spanish
+void agregaPalabra(std::vector<std::pair<QString, QString>> &diccionario, const QString &english, const QString &spanish){
+
+    std::pair<QString, QString> traduccion;
+    traduccion.first = english;
+    traduccion.second = spanish;
+    diccionario.push_back(traduccion);
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::pair<int, QString> significado1;
+    std::pair<int, QString> significado2;
+
+    significado1.first = 1;
+    significado1.second = "One";
+    significado2.first = 2;
+    significado2.second = "Two";
+
+    imprime(significado1);
+    imprime(significado2);
+
+    //creo el vector, de pares, de dos valores QString, llamado english Spanish
+    //crearemos un vector de pair, un diccionario English-Spanish
+    std::vector<std::pair<QString, QString>> englishSpanish;
+
+    agregaPalabra(englishSpanish, "car", "carro");
+    agregaPalabra(englishSpanish, "dog", "perro");
+    agregaPalabra(englishSpanish, "cat", "gato");
+
+    std::vector<std::pair<QString, QString>>::const_iterator i = englishSpanish.cbegin();
+    const std::vector<std::pair<QString, QString>>::const_iterator end = englishSpanish.cend();
+    for (; i != end; ++i)
+    {
+        imprime2(*i);
+    }
+
+    return a.exec();
+}
